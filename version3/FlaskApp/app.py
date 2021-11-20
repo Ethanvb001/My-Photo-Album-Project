9925d07d2ebf1fe76d1b4cc2610af26069db1eff
Ethan <ethanvb001@gmail.com>
2021-11-20 18:10:02 +0200

    Updated 4

---
+++ version3/FlaskApp/app.py
@@ -0,0 +1,83 @@
+from flask import Flask,render_template, request,json
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+"""Routes and get methods"""
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/showSignIn')
+def showSignIn():
+    return render_template('signin.html')
+
+@app.route('/showHome')
+def showHome():
+    return render_template('index.html')
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route("/" ) 
+def main(): 
+    return render_template("index.html")
+
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+
+app.route('/signUp',methods=['POST','GET'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # validate the received values
+        if _name and _email and _password:
+            
+            # All Good, let's call MySQL
+            
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) == 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close() 
+ 
+app.run(host='localhost', port=5000)
